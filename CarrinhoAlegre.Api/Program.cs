2e8c10d77dd94257e4b12184c9756d41974c8241
thiago rodrigues <timen_rodrigues@hotmail.com>
2025-11-18 23:49:55 -0300

    #Cors, Options e Middlewares

--- CarrinhoAlegre.Api/Program.cs
+++ CarrinhoAlegre.Api/Program.cs
@@ -19,6 +19,29 @@ builder.Services.AddOpenApi();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 builder.Services.AddScoped<IProdutoService, ProdutoService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MinhaPoliticaDeCors", policy =>
+    {
+        policy.WithOrigins("http://localhost:4206", "http://localhost:4200") // Quem pode chamar?
+              .AllowAnyHeader() // Quais headers? (Content-Type, Authorization...)
+              .AllowAnyMethod(); // Quais verbos? (GET, POST, PUT...)
+    });
+
+    // Política 1: Aberta (Ex: Para o catálogo de produtos que qualquer site pode ler)
+    options.AddPolicy("PoliticaPublica", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+
+    // Política 2: Restrita (Ex: Para finalizar compra, só seu front-end pode chamar)
+    options.AddPolicy("PoliticaRestrita", policy =>
+    {
+        policy.WithOrigins("https://meufrontend.com") // Só aceita desse site
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -34,6 +57,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("MinhaPoliticaDeCors"); //usa para todos os controllers -> podemos usar o [DisableCors] no controller para 
+//desabilitar essa politica apenas para o controller com a tag
+
+//app.UseCors();//usa a política definida no controller -> No controller -> [EnableCors("PoliticaPublica")], [EnableCors("PoliticaRestrita")],
+//[EnableCors("MinhaPoliticaDeCors")]
 
 app.UseAuthorization();
 
